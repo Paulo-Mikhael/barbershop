8510b3771694a605e0bf63d4e32590d660821c99
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2026-07-12 09:34:57 -0400

    Button component

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+export default function Button({ children, variant = "primary", full = false }: { children: ReactNode, variant?: "primary" | "outlined", full?: boolean }) {
+  return (
+    <button
+      className={
+        clsx(
+          "cursor-pointer",
+          {
+            "px-8 py-4": !full,
+            "w-full h-full": full,
+            "bg-brand-default text-typo-100 hover:bg-brand-dark active:bg-brand-darker focus:bg-brand-bright disabled:bg-typo-400": variant == "primary",
+            "border-brand-default border-2 text-brand-default hover:border-brand-dark hover:text-brand-dark active:border-brand-darker active:text-brand-darker focus:border-brand-bright focus:text-brand-bright disabled:border-typo-400 disabled:text-typo-400": variant == "outlined"
+          }
+        )
+      }
+    >
+      {children}
+    </button>
+  );
+}
