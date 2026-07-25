9848d9f0503f208b5de8785c384dd68bc058d1b6
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2026-07-25 13:59:39 -0400

    Light Mode

--- app/components/Bar/index.tsx
+++ app/components/Bar/index.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 export default function Bar({ full = false, classname }: { full?: boolean, classname?: string }) {
   return (
-    <div className={clsx(`bg-typo-700 h-0.5 ${classname}`, {
+    <div className={clsx(`dark:bg-typo-700 bg-typo-300 h-0.5 ${classname}`, {
       "w-280": !full,
       "w-full": full
     })} />
