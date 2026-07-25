f8dc22fdfaf98bf0ec768da4fe69f00ed3be0921
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2026-07-25 13:04:57 -0400

    HomeMain bold and dark mode improvements

--- app/components/Pages/HomePage/HomeMain/index.tsx
+++ app/components/Pages/HomePage/HomeMain/index.tsx
@@ -9,10 +9,10 @@ export default function HomeMain() {
       <span className="flex flex-col items-center justify-between grow py-6.25">
         <Bar />
         <div className="flex flex-col items-center justify-center gap-8">
-          <h1 className="text-highlight">
+          <h1 className="text-highlight font-bold">
             Barbershop
           </h1>
-          <p className="text-typo-200 max-w-159.25 text-center">
+          <p className="dark:text-typo-200 max-w-159.25 text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis nulla. Phasellus iaculis, diam eget pellentesque auctor, tellus libero posuere leo, sit amet volutpat velit dolor in magna. Aliquam lacinia eros ac turpis tempus, quis gravida est facilisis. Curabitur non erat sit amet sem eleifend tempus nec sed eros.
           </p>
           <Button>
