a6f592750fc9eff8a0a06caf84bd09fa6202da50
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2026-09-06 13:10:34 -0400

    Improve Textbox

--- app/src/components/Textbox/index.tsx
+++ app/src/components/Textbox/index.tsx
@@ -1,21 +1,59 @@
+"use client"
+
 import clsx from "clsx";
+import { LucideIcon } from "lucide-react";
+import { HTMLInputTypeAttribute, ReactNode, useRef } from "react";
 
 type TextboxProps = {
   width?: number,
-  placeholder: string
+  placeholder: string,
+  labelText?: string,
+  LucideIcon?: LucideIcon,
+  ActionButton?: LucideIcon,
+  ActionOnClick?: () => void,
+  type?: HTMLInputTypeAttribute
 }
 
-export default function Textbox({ width, placeholder }: TextboxProps) {
+export default function Textbox({ width, placeholder, labelText, LucideIcon, ActionButton, ActionOnClick, type = "text" }: TextboxProps) {
   const boxWidthClass = `w-[${width}]px`;
+  const input = useRef<HTMLInputElement>(null);
 
   return (
-    <input
-      type="text"
-      placeholder={placeholder}
-      className={clsx("text-caption p-4 bg-box border-2 border-typo-700 outline-0 focus:border-brand-default", {
-        "w-full": !width,
-        [boxWidthClass]: width
-      })}
-    />
+    <span className="flex flex-col gap-2.5">
+      {
+        labelText && (
+          <label>
+            {labelText}
+          </label>
+        )
+      }
+      <span
+        onClick={() => input.current?.focus()}
+        className={
+          clsx("flex p-4 gap-2.5 cursor-pointer bg-box border-2 border-typo-700 outline-0 has-[input:focus]:border-brand-default", {
+            "w-full": !width,
+            [boxWidthClass]: width
+          })}
+      >
+        {
+          LucideIcon && (
+            <LucideIcon />
+          )
+        }
+        <input
+          ref={input}
+          className="outline-0 text-paragraph grow"
+          type={type}
+          placeholder={placeholder}
+        />
+        {
+          ActionButton && (
+            <ActionButton onClick={() => {
+              ActionOnClick && ActionOnClick();
+            }} />
+          )
+        }
+      </span>
+    </span>
   );
 }
